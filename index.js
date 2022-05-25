091cd7ec39c276a6c7216fd0f8ce5db2d8b93958
shubham <singla16shubham@gmail.com>
2022-05-25 23:44:03 +0530

    Added Partials and Layouts

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express=require('express');
 const app=express();
  const port=8001;
 
+ const expressLayouts=require('express-ejs-layouts');
+ app.use(expressLayouts);
+
+ app.use(express.static('./assets'));
+
+//  extract styles and sublayouts in layout
+app.set('layout extractStyles', true);
+app.set('layout Scripts', true);
+
 //  use express router
 app.use('/',require('./routes'))
+
+// Set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 
  app.listen(port,function(err){
