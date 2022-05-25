d69074dd9577106103156e571bbcffdee88b25dc
shubham <singla16shubham@gmail.com>
2022-05-26 00:04:23 +0530

    Connected Database

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express=require('express');
 const app=express();
  const port=8001;
 
+
  const expressLayouts=require('express-ejs-layouts');
+ const db=require('./config/mongoose');
  app.use(expressLayouts);
 
  app.use(express.static('./assets'));
