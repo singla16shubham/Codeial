44101077c30f9cf4d5dbee5a40a2b81d443cc04a
shubham <singla16shubham@gmail.com>
2022-05-27 20:56:58 +0530

    Created User sign-up/sign-in page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,6 +6,18 @@ module.exports.profile=function(req,res)
     });
 }
 // We are adding action
-module.exports.signup=function(req,res){
-    return res.render('user_sign_up',{});
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:'Codeial | SignUp'
+    });
+}
+
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:'Codeial | SignIn'
+    })
+}
+
+module.exports.create=function(req,res){
+    // TODO later;
 }
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,8 @@ const router=express.Router();
 const userController=require('../controllers/users_controller');
 router.get('/profile',userController.profile);
 
+router.get('/sign-Up',userController.signUp);
+router.get('/sign-In',userController.signIn);
+
 
 module.exports=router;
