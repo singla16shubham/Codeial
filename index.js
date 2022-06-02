1748f812f19c0c45d0e2d755e138a6e24758af2b
shubham <singla16shubham@gmail.com>
2022-06-02 23:14:18 +0530

    Added Styles for Header and Footer using SCSS

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const app=express();
 const { urlencoded } = require('express');
 
 app.use(sassMiddleware({
-    src:'/assets/scss',
-    dest:'/assets/css',
+    src:'./assets/scss',
+    dest:'./assets/css',
     debug:true,
     outputStyle:'extended',
     prefix:'/css'
